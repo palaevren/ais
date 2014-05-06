5b4a2ea2b01035a92eae635518290a56c748c931
Evren Pala <evren.pala@agmlab.com>
2014-05-06 21:39:33 +0300

    Random Population generation added. Sort function is BUGGY...

--- ais.c
+++ ais.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
+
+#define POPULATION 100
+#define	OFFSPRING 15
+
+
+
 int points = 0;
 
 typedef struct Location{
@@ -15,14 +22,28 @@ typedef struct Individual{
 	
 }Indv;
 
+typedef struct Rectangle{
+	float min_x;
+	float max_x;
+	float min_y;
+	float max_y;
+	float min_z;
+	float max_z;
+}Obstacle;
+
 FILE* inpF;
 Coordinate* targets;
-int count;
+int COUNT;
 
 void getInput(FILE* f, char* fileName);
 void printCord(Coordinate* crdAr);
-float calculateFitness(Indv* indv);
+double calculateFitness(Indv* indv);
 float euclidianDistance(Coordinate c1, Coordinate c2);
+int* randPerm(int n);
+void printArr(int* arr, int len);
+void swap(int* arr, int idx_a, int idx_b);
+void shuffle(int* arr, int len, int times);
+
 
 int main(int arg, char** argv ){
 	printf("Flight started...\n");
@@ -30,13 +51,14 @@ int main(int arg, char** argv ){
 	printCord(targets);
 	Indv indv;
 	int idx;
-	indv.sequence = (int *) malloc(sizeof(int) * count);
-	for (idx=0; idx<count; idx++){
+	indv.sequence = (int *) malloc(sizeof(int) * COUNT);
+	for (idx=0; idx<COUNT; idx++){
 		indv.sequence[idx] = idx;
 	}
 	calculateFitness(&indv);
 	printf("fitness: %f\n", indv.fitness);
-	//~ count = 1;
+	randPerm(COUNT);
+	//~ COUNT = 1;
 	//~ Coordinate a = {0.4f, -0.2f, 0.5f};
 	//~ Coordinate b = {0.7f, -0.6f, -0.7f};
 	//~ printCord(&a);
@@ -57,23 +79,24 @@ void getInput(FILE* f, char* fileName){
 		targets = (Coordinate*) realloc(targets, (i+1)*sizeof(Coordinate));
 		targets[i] = tmp;
 	}
-	count = i;
+	COUNT = i;
+	return;
 }
 
 void printCord(Coordinate* crdAr){
-	printf("Number of coordinates: %d", count);
+	printf("Number of coordinates: %d", COUNT);
 	int i;
-	for(i=0; i<count; i++){
+	for(i=0; i<COUNT; i++){
 		printf("Coordinate %d, X: %f, Y: %f, Z: %f\n", i, targets[i]._X, targets[i]._Y, targets[i]._Z);
 	}
 }
 
 
-float calculateFitness(Indv* indv){
+double calculateFitness(Indv* indv){
 	int i;
 	double fitness = 0.0;
-	for(i=0; i<count; i++){
-		fitness += (double) euclidianDistance(targets[i], targets[(i+1)%count]);
+	for(i=0; i<COUNT; i++){
+		fitness += (double) euclidianDistance(targets[i], targets[(i+1)%COUNT]);
 	}
 	indv->fitness = fitness;
 	return fitness;
@@ -83,4 +106,84 @@ float euclidianDistance(Coordinate c1, Coordinate c2){
 	return sqrt(pow((sqrt(pow((c1._X-c2._X), 2) + pow((c1._Y-c2._Y), 2))), 2) + pow((c1._Z-c2._Z), 2));
 }
 
+int* randPerm(int n) 
+{	
+	int i,j;
+    int* r = malloc(n * sizeof(int));
+    for(j=0; j<n; j++){
+		r[j] = j;
+	}
+	shuffle(r, n, n);
+	printArr (r, n);
+	return r;
+}
 
+void printArr(int* arr, int len){
+	int i;
+	for(i=0; i< len; i++){
+		printf("%d, ", arr[i]);
+	}
+	printf("\n");
+	return;
+}
+
+void swap(int* arr, int idx_a, int idx_b){
+	printf("%dth element (%d) is to be swapped with %dth element (%d)\n", idx_a, *(arr+idx_a), idx_b, *(arr+idx_b));
+	int tmp = *(arr+idx_a);
+	*(arr+idx_a) = *(arr+idx_b);
+	*(arr+idx_b) = tmp;
+	
+	printf("%dth element (%d) is swapped with %dth element (%d)\n", idx_a, *(arr+idx_a), idx_b, *(arr+idx_b));
+	return;
+}
+
+void shuffle(int* arr, int len, int times){
+	srand(time(NULL));
+	int i;
+	for(i=0; i<times; i++){
+		int index_1 = rand() % len;
+		int index_2 = rand() % len;
+		while(index_2 == index_1){
+			index_2 = rand() % len;
+		}
+		 
+		printf("generated random pair : %d-%d\n", index_1, index_2);
+		swap(arr, index_1, index_2);
+	}
+}
+
+void quickSort( Indv[], int, int);
+int partition( Indv[], int, int);
+
+
+void quickSort( Indv a[], int l, int r)
+{
+   int j;
+
+   if( l < r ) 
+   {
+   	// divide and conquer
+        j = partition( a, l, r);
+       quickSort( a, l, j-1);
+       quickSort( a, j+1, r);
+   }
+	
+}
+
+
+
+int partition( Indv a[], int l, int r) {
+   float pivot, i, j, t;
+   pivot = a[l].fitness;
+   i = l; j = r+1;
+		
+   while( 1)
+   {
+   	do ++i; while( a[i] <= pivot && i <= r );
+   	do --j; while( a[j] > pivot );
+   	if( i >= j ) break;
+   	t = a[i]; a[i] = a[j]; a[j] = t;
+   }
+   t = a[l]; a[l] = a[j]; a[j] = t;
+   return j;
+}
