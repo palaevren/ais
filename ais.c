c515ddebd6d1d27b136335ec79b414f3f1a4158a
palaevren <palaevren@gmail.com>
2014-05-09 02:59:24 +0300

    utility functions written, tested. More test cases should be run and code should be seperated into different files, header and source files also should be seperated

--- ais.c
+++ ais.c
@@ -3,8 +3,8 @@
 #include <math.h>
 #include <time.h>
 
-#define POPULATION 100
-#define	OFFSPRING 15
+#define POPULATION 10
+#define	OFFSPRING 3
 
 
 
@@ -39,25 +39,39 @@ void getInput(FILE* f, char* fileName);
 void printCord(Coordinate* crdAr);
 double calculateFitness(Indv* indv);
 float euclidianDistance(Coordinate c1, Coordinate c2);
-int* randPerm(int n);
-void printArr(int* arr, int len);
-void swap(int* arr, int idx_a, int idx_b);
-void shuffle(int* arr, int len, int times);
-
+void randPerm(int *i, int n);
+void printArr(int *arr, int len);
+void swap(int *arr, int idx_a, int idx_b);
+void shuffle(int *arr, int len, int times);
+void quickSort(Indv *, int, int);
+int partition(Indv *, int, int);
+void printIndvFits(Indv *indv, int size);
+void generateIndv(Indv *ind);
 
 int main(int arg, char** argv ){
+	int i;
 	printf("Flight started...\n");
 	getInput(inpF, "inp.txt");
 	printCord(targets);
-	Indv indv;
-	int idx;
-	indv.sequence = (int *) malloc(sizeof(int) * COUNT);
-	for (idx=0; idx<COUNT; idx++){
-		indv.sequence[idx] = idx;
+	Indv* indv = (Indv *) malloc(sizeof(Indv)*POPULATION);
+	
+	for(i=0; i<POPULATION; i++){
+		//~ generateIndv(indv+i);
+		(indv+i)->sequence = (int *) malloc(sizeof(int)*COUNT);
+		randPerm((indv+i)->sequence, COUNT);
+		(indv+i)->fitness = calculateFitness(indv+i);
 	}
-	calculateFitness(&indv);
-	printf("fitness: %f\n", indv.fitness);
-	randPerm(COUNT);
+	printIndvFits(indv, POPULATION);
+	//~ int idx;
+	//~ indv.sequence = (int *) malloc(sizeof(int) * COUNT);
+	//~ for (idx=0; idx<COUNT; idx++){
+		//~ indv.sequence[idx] = idx;
+	//~ }
+	
+	//indv->sequence = randPerm(COUNT);
+	//calculateFitness(indv);
+	//~ printf("fitness: %f\n", indv->fitness);
+	
 	//~ COUNT = 1;
 	//~ Coordinate a = {0.4f, -0.2f, 0.5f};
 	//~ Coordinate b = {0.7f, -0.6f, -0.7f};
@@ -77,13 +91,13 @@ void getInput(FILE* f, char* fileName){
 		fscanf(f, "%f", &(tmp._Y));
 		fscanf(f, "%f", &(tmp._Z));
 		targets = (Coordinate*) realloc(targets, (i+1)*sizeof(Coordinate));
-		targets[i] = tmp;
+		*(targets+i) = tmp;
 	}
 	COUNT = i;
 	return;
 }
 
-void printCord(Coordinate* crdAr){
+void printCord(Coordinate *crdAr){
 	printf("Number of coordinates: %d", COUNT);
 	int i;
 	for(i=0; i<COUNT; i++){
@@ -92,13 +106,14 @@ void printCord(Coordinate* crdAr){
 }
 
 
-double calculateFitness(Indv* indv){
+double calculateFitness(Indv *indv){
 	int i;
 	double fitness = 0.0;
+	int* seq = indv->sequence;
 	for(i=0; i<COUNT; i++){
-		fitness += (double) euclidianDistance(targets[i], targets[(i+1)%COUNT]);
+		fitness += (double) euclidianDistance(targets[seq[i]], targets[seq[(i+1)%COUNT]]);
 	}
-	indv->fitness = fitness;
+	//~ indv->fitness = fitness;
 	return fitness;
 }
 
@@ -106,19 +121,19 @@ float euclidianDistance(Coordinate c1, Coordinate c2){
 	return sqrt(pow((sqrt(pow((c1._X-c2._X), 2) + pow((c1._Y-c2._Y), 2))), 2) + pow((c1._Z-c2._Z), 2));
 }
 
-int* randPerm(int n) 
+void randPerm(int *r, int n) 
 {	
 	int i,j;
-    int* r = malloc(n * sizeof(int));
+    //~ r = malloc(n * sizeof(int));
     for(j=0; j<n; j++){
-		r[j] = j;
+		*(r+j) = j;
 	}
 	shuffle(r, n, n);
 	printArr (r, n);
-	return r;
+	return;
 }
 
-void printArr(int* arr, int len){
+void printArr(int *arr, int len){
 	int i;
 	for(i=0; i< len; i++){
 		printf("%d, ", arr[i]);
@@ -127,63 +142,82 @@ void printArr(int* arr, int len){
 	return;
 }
 
-void swap(int* arr, int idx_a, int idx_b){
-	printf("%dth element (%d) is to be swapped with %dth element (%d)\n", idx_a, *(arr+idx_a), idx_b, *(arr+idx_b));
+void swap(int *arr, int idx_a, int idx_b){
+	//~ printf("%dth element (%d) is to be swapped with %dth element (%d)\n", idx_a, *(arr+idx_a), idx_b, *(arr+idx_b));
 	int tmp = *(arr+idx_a);
 	*(arr+idx_a) = *(arr+idx_b);
 	*(arr+idx_b) = tmp;
 	
-	printf("%dth element (%d) is swapped with %dth element (%d)\n", idx_a, *(arr+idx_a), idx_b, *(arr+idx_b));
+	//printf("%dth element (%d) is swapped with %dth element (%d)\n", idx_a, *(arr+idx_a), idx_b, *(arr+idx_b));
 	return;
 }
 
-void shuffle(int* arr, int len, int times){
-	srand(time(NULL));
+void shuffle(int *arr, int len, int times){
 	int i;
 	for(i=0; i<times; i++){
+		srand(time(NULL));
 		int index_1 = rand() % len;
+		srand(time(NULL));
 		int index_2 = rand() % len;
 		while(index_2 == index_1){
+			srand(time(NULL));
 			index_2 = rand() % len;
 		}
 		 
-		printf("generated random pair : %d-%d\n", index_1, index_2);
+		//printf("generated random pair : %d-%d\n", index_1, index_2);
 		swap(arr, index_1, index_2);
 	}
 }
 
-void quickSort( Indv[], int, int);
-int partition( Indv[], int, int);
-
-
-void quickSort( Indv a[], int l, int r)
+void quickSort( Indv *a, int l, int r)
 {
    int j;
 
    if( l < r ) 
    {
    	// divide and conquer
-        j = partition( a, l, r);
-       quickSort( a, l, j-1);
-       quickSort( a, j+1, r);
+		j = partition( a, l, r);
+		quickSort( a, l, j-1);
+		quickSort( a, j+1, r);
    }
 	
 }
 
 
 
-int partition( Indv a[], int l, int r) {
-   float pivot, i, j, t;
-   pivot = a[l].fitness;
-   i = l; j = r+1;
+int partition( Indv *a, int l, int r) {
+	float pivot;
+	int i, j;
+	//~ int* t = (int*) malloc(sizeof(int));
+	Indv* t = (Indv *) malloc(sizeof(Indv));
+	pivot = (a+l)->fitness;
+	i = l;
+	j = r+1;
+	
+	while( 1){
+		do ++i; while( (a+i)->fitness<= pivot && i <= r );
+		do --j; while( (a+j)->fitness > pivot );
+		if( i >= j ) break;
+		*t = *(a+i);
+		*(a+i) = *(a+j);
+		*(a+j) = *t;
 		
-   while( 1)
-   {
-   	do ++i; while( a[i] <= pivot && i <= r );
-   	do --j; while( a[j] > pivot );
-   	if( i >= j ) break;
-   	t = a[i]; a[i] = a[j]; a[j] = t;
-   }
-   t = a[l]; a[l] = a[j]; a[j] = t;
-   return j;
+	}
+	*t = *(a+l);
+	*(a+l) = *(a+j);
+	*(a+j) = *t;
+	return j;
+}
+
+void generateIndv(Indv *ind){
+	 randPerm(ind->sequence, COUNT);
+	calculateFitness(ind);
+} 
+
+void printIndvFits(Indv *indv, int size){
+	int i;
+	for(i=0; i<size; i++){
+		printf("addr=%p and fitness=%lf, permutation=", indv+i, (indv+i)->fitness);
+		printArr((indv+i)->sequence, COUNT);
+	}
 }
